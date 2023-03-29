6d21f4f15414af4615c6b3d244bbf8ec9a227624
rramires <ricardo@flexbr.com>
2023-03-29 16:16:28 -0300

    Init node project

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {"id": 1, "product": "Laptop", "price": 1850.00},
+    {"id": 2, "product": "Tablet", "price": 980.00},
+    {"id": 3, "product": "Cellphone", "price": 1180.00}
+  ]);
 });
 
 app.listen(3000, () => {
